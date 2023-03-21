83dbf166db4fc010b1e5ed632fcfb4ae4f7554f4
Nishant Kishore <nishantk3121@gmail.com>
2023-03-21 12:39:58 +0530

    Code can now display the results belong to which  specific page

--- BookStoreScrapping.py
+++ BookStoreScrapping.py
@@ -2,32 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 dict={'One':1,'Two':2,'Three':3,'Four':4,'Five':5}
 j=1
-while(True):
-    r=requests.get(f'http://books.toscrape.com/catalogue/page-{j}.html').text
-    soup=BeautifulSoup(r,'lxml')
-    soup.prettify()
-    if(soup.title.text=='404 Not Found'):
-            print('End of Result')
-            break
+with open(f'BookswithFiveandFourStarRating.txt','w',encoding='utf-8') as f:
+    while(True):
+        r=requests.get(f'http://books.toscrape.com/catalogue/page-{j}.html').text
+        soup=BeautifulSoup(r,'lxml')
+        soup.prettify()
+        if(soup.title.text=='404 Not Found'):
+                print('End of Result')
+                break
 
-    ratings=soup.findAll('article', class_='product_pod')
-    names=soup.findAll('h3')
-    links = soup.findAll('h3')
-    prices = soup.findAll('p', class_='price_color')
-    c=1
-    for index,rating in enumerate(ratings):
-        name=(names[index].a['title'])
-        stars=(ratings[index].p['class'][1])
-        link = links[index].a['href']
-        price = prices[index].text[1:]
-        if(dict[stars]>=4):
-            with open(f'BookswithFiveandFourStarRating.txt','a',encoding='utf-8') as f:
-                f.write(f'Book Name: {name}\n Book Price: {price}\n Book Link:{link}\n')
-                f.write('\n')
-                f.close()
-    print(f'Page {j} saved\n')
-    c=c+1
-    j=j+1
+        ratings=soup.findAll('article', class_='product_pod')
+        names=soup.findAll('h3')
+        links = soup.findAll('h3')
+        prices = soup.findAll('p', class_='price_color')
+        c=1
+        for index,rating in enumerate(ratings):
+            name=(names[index].a['title'])
+            stars=(ratings[index].p['class'][1])
+            link = links[index].a['href']
+            price = prices[index].text[1:]
+            if(dict[stars]>=4):
+                    f.write(f'Book Name: {name}\n Book Price: {price}\n Book Link:{link}\n')
+                    f.write('\n')
+        f.write(f'-------------------------------End of results from PAGE {j}----------------------------------------\n')
+        print(f'Page {j} saved\n')
+        c=c+1
+        j=j+1
+f.close()
 
 
 
